731705b3a53ecc72ee88ec3dec7669a5a5cb9306
CTMDOVZ <132113148+CTMDOVZ@users.noreply.github.com>
2023-10-16 16:52:17 -0500

    Update PersonController.java

--- demo/src/main/java/com/example/demo/Application/PersonController.java
+++ demo/src/main/java/com/example/demo/Application/PersonController.java
@@ -1,5 +1,27 @@
 package com.example.demo.Application;
 
-public class PersonController {
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+@RestController
+@RequestMapping("/groups")
+public class GroupController {
+
+    @Autowired
+    private GroupRepository groupRepository;
     
+    @GetMapping
+    public List<Group> getAllGroups() {
+        return groupRepository.getAllGroups();
+    }
+
+    
+    @GetMapping("/{groupId}")
+    public Group getGroupById(@PathVariable int groupId) {
+        return groupRepository.getGroupById(groupId);
+    }
+
+    
+
 }
