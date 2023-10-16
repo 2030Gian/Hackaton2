a7a0bd32eee5b5417c480264e0cbf6eedfbbbaad
CTMDOVZ <132113148+CTMDOVZ@users.noreply.github.com>
2023-10-16 17:06:29 -0500

    Update GroupController.java

--- demo/src/main/java/com/example/demo/Application/GroupController.java
+++ demo/src/main/java/com/example/demo/Application/GroupController.java
@@ -8,16 +8,15 @@ import org.springframework.web.bind.annotation.*;
 public class GroupController {
 
     @Autowired
-    private GroupRepository groupRepository; // Suponiendo que tienes un repositorio llamado GroupRepository
-
-    // Obtener todas las personas en un grupo específico
+    private GroupRepository groupRepository;
+    
     @GetMapping("/{groupId}/persons")
     public List<Person> getPersonsInGroup(@PathVariable Long groupId) {
         Group group = groupRepository.findById(groupId).orElse(null);
         if (group != null) {
             return group.getMembers();
         } else {
-            // Manejar el caso en que no se encuentre el grupo
+         
             return Collections.emptyList();
         }
     }
